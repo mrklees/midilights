40eece95638a331e4856e0ae6578161112401e2b
Alex Perusse <perusse.a@gmail.com>
2022-02-27 11:44:50 -0800

    keep retrying com port

--- midilights/midilights.py
+++ midilights/midilights.py
@@ -1,8 +1,9 @@
 import sys
-
+import time
 import mido
 
 from serial_wrapper import SerialWrapper, get_available_ports
+from serial.serialutil import SerialException
 
 def display_input_ports():
     inports = mido.get_input_names()
@@ -24,16 +25,32 @@ def bridge_midi_to_serial(in_port, device="COM3"):
                 srl.send_data(data)
 
 
-            
-
-if __name__ == "__main__":
+def run_bridge():
     guess = display_input_ports()
     if guess:
         inport_ix = guess
     else:
         inport_ix = int(input("port:"))
+
     inport = mido.get_input_names()[inport_ix]
-    available_ports = get_available_ports()
+    for i in range(15):
+        available_ports = get_available_ports()
+        if available_ports:
+            break
+        print("Waiting for COM port to appear")
+        time.sleep(1)
+    else:
+        assert False, "Never found COM port"
+
     first_port = list(available_ports.keys())[0]
-    print(f"Sending Midi to {first_port}")
+    print(f"Sending Midi from {inport_ix} to {first_port}")
     bridge_midi_to_serial(inport, device=first_port)
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            run_bridge()
+        except SerialException:
+            print("Caught SerialException will restart in 2 second")
+            time.sleep(2)
